2bfc512fa8c7cc3ed33ee7d23d841c10ea81d045
Alex Fernandez <olorenzo@outlook.com>
2016-06-27 02:27:20 -0600

    remove configs

--- server/server.js
+++ server/server.js
@@ -5,8 +5,7 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  var port = process.env.PORT || 3000;
-  return app.listen(port,function() {
+  return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
